57103e6cb0d567867348074c90785b35db30e6c7
David C Wang <dcwangmit01@gmail.com>
2018-09-10 23:19:43 +0000

    Added creation of CNAME necessary for github hosting

--- bin/publish_to_ghpages.sh
+++ bin/publish_to_ghpages.sh
@@ -26,5 +26,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo "Adding CNAME"
+echo "kdf.csco.cloud" > public/CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
